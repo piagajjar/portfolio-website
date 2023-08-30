3b9961e65b6890561a1cb867785f96a76fa7c4b6
piagajjar <pia17492@gmail.com>
2023-08-30 01:10:53 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hey, Welcome to Pia's Portfolio!");
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
